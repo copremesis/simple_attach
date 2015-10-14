d9d2d0fb8dab8bf1e49e21917de6b46f753a6137
Rob Ortiz <rob@cabforward.com>
2015-10-14 05:13:02 -0500

    adding bootstrap and handling multiple file uploads

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -3,6 +3,8 @@ class UploadController < ApplicationController
     #forward to view for now
     #build list of attachments
     @files = `ls #{Rails.root.join('public','uploads')}`.split(/\n/) 
+
+    render :layout => 'bootstrap'
   end
 
   #POST to this action will get the file
@@ -10,11 +12,21 @@ class UploadController < ApplicationController
   
   def attach
     #simplest form of attachment
-    uploaded_io = params[:upload][:file]
-    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
+
+
+    (params[:files] || []).each {|uploaded_io|
+      File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
+    }
+
     #something happens here like book keeping or conversion etc
     redirect_to :root
   end
+
+
+
+
+
+
 end
